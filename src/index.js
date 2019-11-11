d3b61953500ff36584e1d9dbc7ec31be52a7d694
Elliot Winkler <elliot.winkler@gmail.com>
2019-11-11 00:53:15 -0500

    Don't import CSS via JS
    Remember that we have to generate Elm style files from CSS module files
    in order to be able to import those styles from within Elm. I couldn't
    figure out exactly why, but this process seems to work better when
    running `parcel build` vs. `parcel serve` if we import the CSS files in
    HTML and CSS rather than in JS. This is necessary in order to be able to
    run the Cypress tests on CI.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import { Elm } from "./Main.elm";
 
-import "reset.css";
-import "./index.css";
-import "./styles/main.css";
-import "./styles/node-content.css";
-
 class GraphNode {
   constructor(nodeId, element) {
     this.nodeId = nodeId;
